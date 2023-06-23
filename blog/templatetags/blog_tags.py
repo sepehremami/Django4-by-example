cd5936cbf1b99a6a90096800df8156a74b492dd5
sepehremami <sepehr7890@gmail.com>
2023-06-23 08:11:01 -0400

    most commented post

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,24 @@
+from django import template
+from django.db.models import Count
+
+from ..models import Post
+
+register = template.Library()
+
+
+@register.simple_tag(name='total_post')
+def total_post():
+    return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_post': latest_posts}
+
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+        total_comment=Count('comments')
+    ).order_by('-total_comment')[:count]
