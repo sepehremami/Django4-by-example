02ec8167ffafd93428b7d5be68a5035fad120371
Sepehr <sepehr7890@gmail.com>
2023-07-17 21:16:20 +0430

    trigramsimilarity search

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from taggit.models import Tag
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from .forms import EmailPostForm, CommentForm, SearchForm
@@ -101,12 +101,9 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', 'body')
-            search_query = SearchQuery(query)
             results = Post.objects.annotate(
-                search=search_vector,
-                rank=SearchRank(search_vector, search_query)
+                similarity=TrigramSimilarity('title', query)
                 ) \
-                .filter(search=search_query).order_by('-rank') # select to_tsvector('title', 'body') @@ to_tsquery(query);
+                .filter(similarity__gt=0.1).order_by('-similarity') # select to_tsvector('title', 'body') @@ to_tsquery(query);
     return render(request,'blog/post/search.html', {'form':form, 'query':query, 'results':results})
 
